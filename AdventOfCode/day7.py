fbbe8b836803f4feac9528f8b6bd0b2618e2faed
Alan Matthews <alan.r.matthews@gmail.com>
2024-12-22 18:07:29 -0500

    Speed up Day 7

--- AdventOfCode/day7.py
+++ AdventOfCode/day7.py
@@ -1,18 +1,22 @@
 """ Advent of Code 2024 Day 2 """
 
 import re
-import itertools
 import operator
 
 
-def is_possible(values: list[int], operators: list) -> bool:
+def is_possible(goal: int, values: list[int], operators: list, accumulator: int) -> bool:
     """Returns True if an equation is possible, False otherwise."""
-    for permutation in itertools.product(operators, repeat=len(values) - 2):
-        result = values[1]
-        for i in range(len(values) - 2):
-            result = permutation[i](result, values[i + 2])
 
-        if result == values[0]:
+    if len(values) == 0:
+        return accumulator == goal
+
+    for op in operators:
+        temp = op(accumulator, values[0])
+
+        if temp > goal:
+            continue
+
+        if is_possible(goal, values[1:], operators, temp):
             return True
 
     return False
@@ -23,29 +27,25 @@ def concat(a: int, b: int) -> int:
     return int(f"{a}{b}")
 
 
-def part1(input_file: str) -> str:
-    """Returns the solution for part 1 of the day."""
+def compute(input_file: str, operators: list) -> str:
+    """Computes the solution for the given input file and operators."""
     num_possible = 0
     with open(input_file, "r", encoding="utf-8") as file:
         for line in file:
             m = re.findall(r"(\d+)", line.rstrip())
             values = list(map(int, m))
 
-            if is_possible(values, [operator.add, operator.mul]):
+            if is_possible(values[0], values[2:], operators, values[1]):
                 num_possible += values[0]
 
     return str(num_possible)
+
+
+def part1(input_file: str) -> str:
+    """Returns the solution for part 1 of the day."""
+    return compute(input_file, [operator.add, operator.mul])
 
 
 def part2(input_file: str) -> str:
     """Returns the solution for part 2 of the day."""
-    num_possible = 0
-    with open(input_file, "r", encoding="utf-8") as file:
-        for line in file:
-            m = re.findall(r"(\d+)", line.rstrip())
-            values = list(map(int, m))
-
-            if is_possible(values, [operator.add, operator.mul, concat]):
-                num_possible += values[0]
-
-    return str(num_possible)
+    return compute(input_file, [operator.add, operator.mul, concat])
